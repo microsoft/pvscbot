2bcd6fb60927c2148c995a771377973564db81a1
Brett Cannon <brcan@microsoft.com>
2018-12-13 15:32:56 -0800

    Update label text

--- pvscbot/github/labels.py
+++ pvscbot/github/labels.py
@@ -5,7 +5,7 @@ class Status(enum.Enum):
     classify = "classify"
     triage = "triage"
     needs_decision = "needs decision"
-    needs_more_info = "needs more info"
+    needs_more_info = "info needed"
     needs_PR = "needs PR"
     needs_spec = "needs spec"
     needs_upstream_fix = "needs upstream fix"
