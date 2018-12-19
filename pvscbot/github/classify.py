a8830007f25644dbea5bc314030ba52b7e82009b
Brett Cannon <brcan@microsoft.com>
2018-12-19 14:19:46 -0800

    Add more classify tests

--- pvscbot/github/classify.py
+++ pvscbot/github/classify.py
@@ -5,6 +5,29 @@ from . import labels
 router = gidgethub.routing.Router()
 
 
+def is_opened(event):
+    return event.data["issue"]["state"] == "open"
+
+
+def has_classify(event):
+    return any(
+        label["name"] == labels.Status.classify.value
+        for label in event.data["issue"]["labels"]
+    )
+
+
+async def add_classify_label(gh, event):
+    await gh.post(
+        event.data["issue"]["labels_url"],
+        data={"labels": [labels.Status.classify.value]},
+    )
+
+
+def has_labels(event):
+    return event.data["issue"]["labels"]
+
+
+# Removing 'classify' from closed issues is taken care of in the 'closed' submodule.
 @router.register("issues", action="opened")
 @router.register("issues", action="reopened")
 async def classify_new_issue(event, gh, *args, **kwargs):
@@ -18,4 +41,22 @@ async def classify_new_issue(event, gh, *args, **kwargs):
     if issue["labels"]:
         # Teammate pre-classified the issue when creating it.
         return
-    await gh.post(issue["labels_url"], data={"labels": [labels.Status.classify.value]})
+    await add_classify_label(gh, event)
+
+
+@router.register("issues", action="labeled")
+async def added_label(event, gh, *args, **kwargs):
+    if not is_opened(event):
+        return
+    elif has_classify(event):
+        await gh.delete(
+            event.data["issue"]["labels_url"], {"name": labels.Status.classify.value}
+        )
+
+
+@router.register("issues", action="unlabeled")
+async def no_labels(event, gh, *args, **kwargs):
+    if not is_opened(event):
+        return
+    elif not has_labels(event):
+        await add_classify_label(gh, event)
--- pvscbot/tests/test_github/test_classify.py
+++ pvscbot/tests/test_github/test_classify.py
@@ -11,10 +11,15 @@ from ...github import classify, labels
 class FakeGH:
     def __init__(self):
         self.post_ = []
+        self.delete_ = []
 
     async def post(self, url, url_vars={}, *, data):
         post_url = gidgethub.sansio.format_url(url, url_vars)
         self.post_.append((post_url, data))
+
+    async def delete(self, url, url_vars={}):
+        delete_url = gidgethub.sansio.format_url(url, url_vars)
+        self.delete_.append(delete_url)
 
 
 @pytest.mark.asyncio
@@ -25,7 +30,7 @@ async def test_new_issue_with_no_labels():
     event = gidgethub.sansio.Event(sample_data, event="issues", delivery_id="12345")
     gh = FakeGH()
 
-    await classify.add_classify_label(event, gh)
+    await classify.classify_new_issue(event, gh)
     assert len(gh.post_) == 1
     action = gh.post_[0]
     assert (
@@ -45,6 +50,53 @@ async def test_new_issue_with_labels():
 
     await classify.classify_new_issue(event, gh)
     assert not len(gh.post_)
+
+
+@pytest.mark.asyncio
+async def test_removing_classify_label():
+    sample_data = json.loads(
+        importlib_resources.read_text(samples, "issues-labeled-has_classify.json")
+    )
+    event = gidgethub.sansio.Event(sample_data, event="issues", delivery_id="1")
+    gh = FakeGH()
+
+    await classify.router.dispatch(event, gh)
+    assert len(gh.delete_) == 1
+    assert (
+        gh.delete_[0]
+        == "https://api.github.com/repos/Microsoft/vscode-python/issues/3327/labels/classify"
+    )
+
+
+@pytest.mark.asyncio
+async def test_adding_classify_label_again():
+    sample_data = json.loads(
+        importlib_resources.read_text(samples, "issues-unlabeled-no_labels.json")
+    )
+    event = gidgethub.sansio.Event(sample_data, event="issues", delivery_id="1")
+    gh = FakeGH()
+
+    await classify.router.dispatch(event, gh)
+    assert len(gh.post_) == 1
+    assert gh.post_[0] == (
+        "https://api.github.com/repos/Microsoft/vscode-python/issues/3327/labels",
+        {"labels": [labels.Status.classify.value]},
+    )
+
+
+@pytest.mark.asyncio
+async def test_no_adding_label_on_closed_issue():
+    sample_data = json.loads(
+        importlib_resources.read_text(
+            samples, "issues-unlabeled-closed_with_no_labels.json"
+        )
+    )
+    event = gidgethub.sansio.Event(sample_data, event="issues", delivery_id="1")
+    gh = FakeGH()
+
+    await classify.router.dispatch(event, gh)
+    assert not gh.post_
+    assert not gh.delete_
 
 
 # XXX Test routing; @pytest.mark.parametrize('action', ['opened', 'reopened', 'synchronize'])
