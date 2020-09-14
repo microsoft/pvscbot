ef6920ec5ecbfac90e5a16b50edb264dceffd4f6
Brett Cannon <brett@python.org>
2020-09-14 16:18:09 -0700

    Add the "investigating" label (#58)

--- __app__/github/labels.py
+++ __app__/github/labels.py
@@ -8,6 +8,7 @@ import enum
 class Status(enum.Enum):
     classify = "classify"
     triage = "triage"
+    investigating = "investigating"
     needs_upstream_fix = "needs upstream fix"
     needs_decision = "needs decision"
     needs_spike = "needs spike"
